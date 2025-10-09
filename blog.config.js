d2d5b9323e78fd37ed5cb4e2165c17e687415ecf
Friedrich Huang <138540299+Friedrich-233@users.noreply.github.com>
2025-10-09 16:03:26 +0100

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -65,7 +65,7 @@ const BLOG = {
   // 欢迎语打字效果,Hexo,Matery主题支持, 英文逗号隔开多个欢迎语。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    '回英国上学了, 欢迎来看我摸鱼',
+    '回伦敦上学了, 欢迎来看我摸鱼, 修Blog修了半天',
 
   // uuid重定向至 slug
   UUID_REDIRECT: process.env.UUID_REDIRECT || false
